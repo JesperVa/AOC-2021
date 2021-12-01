c870f7f2b8d10de33a239a7b6f2faa575e937f37
Jesper Vallett <jesper@vallett.se>
2021-12-01 17:22:23 +0100

    Day 1

---
+++ 01/Gold.kts
@@ -0,0 +1,10 @@
+import java.io.File
+
+val queue = ArrayDeque<Int>(3)
+var count : Int = 0
+File("goldinput.txt").forEachLine {
+    val value = it.toInt()
+    if (queue.size == 3 && queue.removeFirst() < value) count += 1
+    queue.add(value)
+}
+println(count)
---
+++ 01/Silver.kts
@@ -0,0 +1,8 @@
+import java.io.File
+
+println(
+    File("silverinput.txt").useLines { l -> l.map { it.toInt() }
+        .zipWithNext()
+        .count{ (p,c) -> p < c }
+    }
+)
